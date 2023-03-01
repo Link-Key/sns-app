507b3c33c331dc1f41ded685f410f325bd3a426e
JensenLyou <jassenlyou@foxmail.com>
2023-03-01 12:32:38 +0800

    feat:upgrade

--- src/App.js
+++ src/App.js
@@ -134,7 +134,7 @@ export const APP_DATA = gql`
 const upgradeMode = true
 
 export const upgradeTIme = () => {
-  const date = new Date('2023-03-01 12:00:00').toUTCString()
+  const date = new Date('2023-03-01 15:00:00').toUTCString()
   return date
 }
 
